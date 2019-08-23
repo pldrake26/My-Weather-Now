3ff1c71012b63af24e727d9bfda81f27db29891b
pldrake26 <pldrake26@outlook.com>
2019-08-23 18:12:22 -0500

    Moved current conditions code into it's own component.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header.js';
+import CurrentConditions from './components/CurrentConditions.js';
 import Footer from './components/Footer.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  componentDidMount() {
-    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?zip=35906,us&units=imperial&APPID=${process.env.REACT_APP_API_KEY}`;
-    fetch(
-      apiUrl
-    ).then(res => res.json())
-    .then(
-      (result) => {
-        this.setState({
-          name: result.name,
-          temp: result.main.temp,
-          skies: result.weather[0].main,
-          skiesIcon: result.weather[0].icon,
-          cloudiness: result.clouds.all,
-          wind: result.wind.speed,
-          humidity: result.main.humidity,
-          sunrise: result.sys.sunrise,
-          sunset: result.sys.sunset
-        });
-      })
-  }
   render() {
-    const icon = this.state.skiesIcon;
-    const iconPrefetch = `http://openweathermap.org/img/w/${icon}.png`;
-    const sunriseTime = this.state.sunrise;
-    const sunsetTime = this.state.sunset;
     return (
       <div className="App">
         <Header />
-        <div style={{margin: '3em', color: '#2065e6'}}>
-          <h2>{this.state.name}, AL</h2>
-          <p>Data fetch time: {new Date().toLocaleDateString()}&nbsp;{new Date().toLocaleTimeString()}</p>
-          <p><span style={{fontSize: '3em'}}>{Math.round(this.state.temp)}&deg;&nbsp;F&nbsp;/&nbsp;</span>{this.state.skies}<img src={iconPrefetch} alt="Sky Icon" width="75px" height="auto"/></p>
-          <h3>Current Conditions:</h3>
-          <ul>
-            <li>Cloud Coverage: {this.state.cloudiness}&#37;</li>
-            <li>Wind speed: {this.state.wind}&nbsp;Mph</li>
-            <li>Humidity: {this.state.humidity}&#37;</li>
-            <li>Sunrise: {new Date(sunriseTime * 1000).toLocaleTimeString()}</li>
-            <li>Sunset: {new Date(sunsetTime * 1000).toLocaleTimeString()}</li>
-          </ul>
-        </div>
+        <CurrentConditions />
         <Footer />
       </div>
     );
