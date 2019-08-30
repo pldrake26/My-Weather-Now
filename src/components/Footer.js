49ed3d74d99114c31f6915b4b33c881a0eb6ff0f
pldrake26 <pldrake26@outlook.com>
2019-08-30 01:16:34 -0500

    Small style changes and footer text changes.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
     return (
       <div style={{ margin: "3em", color: "#2065e6" }}>
         <p>
-          &copy;2019 - Coded by Phillip L. Drake. Data provided by&nbsp;<br/>
+          &copy;2019 - Coded by Phillip L. Drake.<br/>
           <a href="https://darksky.net" target="_blank" rel="noopener noreferrer"><img src="/img/powered_by_darksky.png" alt="Powered by Dark Sky Weather API" width="400" height="auto"/></a>
         </p>
       </div>
