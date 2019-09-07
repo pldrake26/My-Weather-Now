805879975395473e48350f9a02b10dabafc6d7bc
pldrake26 <pldrake26@outlook.com>
2019-09-06 19:20:22 -0500

    Minor style changes.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,6 @@ class Footer extends Component {
       <div style={{ margin: "3em", color: "#2065e6" }}>
         <p>
           &copy;2019 - Coded by Phillip L. Drake.<br/>
-          <a href="https://darksky.net" target="_blank" rel="noopener noreferrer"><img src="/img/powered_by_darksky.png" alt="Powered by Dark Sky Weather API" width="400" height="auto"/></a>
         </p>
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,17 @@ class Header extends Component {
     const headerStyle = {
       backgroundColor: "#607d8b",
       padding: "1em 3em",
-      color: "#2065e6"
+      color: "#2065e6",
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))'
     };
     return (
       <div>
         <header className="App-header" style={headerStyle}>
           <h1 style={{color: 'white'}}>My Weather NOW</h1>
+          <div>
+            <a href="https://darksky.net" target="_blank" rel="noopener noreferrer"><img src="/img/powered_by_darksky.png" alt="Powered by Dark Sky Weather API" width="400" height="auto"/></a>
+          </div>
         </header>
       </div>
     );
